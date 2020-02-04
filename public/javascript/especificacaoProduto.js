b0f049796201957e6be15f4acbd4bb5a9601fae4
nosdeedson <josemariazumira@gmail.com>
2020-02-03 23:22:15 -0300

    pagina produto pronta

--- public/javascript/especificacaoProduto.js
+++ public/javascript/especificacaoProduto.js
@@ -1,13 +1,18 @@
 function paginaProduto(){
 
-    document.querySelectorAll('a').forEach( link =>{
+    document.querySelectorAll('[edlink]').forEach( link =>{
         const conteudo = document.getElementById('produtos')
         link.onclick = (evento)=>{
             evento.preventDefault()
             
             fetch(link.href)
                 .then( resposta => resposta.text())
-                .then(html => conteudo.innerHTML = '<di> teste</div>')
+                .then(html => conteudo.innerHTML = html)
         }
     })
+}
+
+function configurarVideo(){
+    const video = document.getElementById("meuVideo")
+    video.oncontextmenu = () => false
 }
