0a3ff36d706ad9460724f1bda459cc27ab60396e
nosdeedson <josemariazumira@gmail.com>
2020-02-08 19:13:54 -0300

    apaguei codigos comentadso

--- public/javascript/especificacaoProduto.js
+++ public/javascript/especificacaoProduto.js
@@ -11,17 +11,6 @@ function paginaProduto(){
     }
    })
 
-/*
-    document.querySelectorAll('edlink').forEach( link =>{
-        const conteudo = document.getElementById('produtos')
-        link.onclick = (evento)=>{
-            evento.preventDefault()
-            
-            fetch(link.href)
-                .then( resposta => resposta.text())
-                .then(html => conteudo.innerHTML = html)
-        }
-    })*/
 }
 
 function mudaImagem(id){
